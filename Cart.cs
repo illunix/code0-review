9a0128e8fe978506984451c30d05b8912eb46e99
cros448 <cros448@gmail.com>
2021-01-16 03:59:21 +0100

    update

---
+++ Cart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ravency
+{
+    public class Cart
+    {
+        public IList<Product> Products { get; init; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var product in Products)
+                {
+                    totalPrice += product.Price * product.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+
+        public record Product
+        {
+            public string Name { get; init; }
+            public string ImageUrl { get; init; }
+            public decimal Price { get; init; }
+            public int Quantity { get; init; }
+        }
+    }
+}
